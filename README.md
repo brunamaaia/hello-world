f6d78ee06c9b447b43e853cfced08489ae63d194
Bruna Maia <stayinjojopose@gmail.com>
2024-05-15 20:29:15 -0300

    k

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# hello-world
+# hello-world 
+Bruna Maia Garda
+Amo meu gatinho Paçoca :)
---
+++ programa.py
@@ -0,0 +1,8 @@
+x = 12
+y = int(input("Digite o valor de y: "))
+if y > x:
+    conta = y + x
+    print(conta)
+else:
+    conta2 = x - y
+    print(conta2)
